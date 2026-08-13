fd12b4113af760d4b3a32a3947b0c606ee349c2c
ahmad khawam <ahmadkhawam99@gmail.com>
2026-08-13 17:05:25 +0300

    chore: initialize Flutter project

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Store App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(body: Center(child: Text('Store App'))),
+    );
+  }
+}
